0545a37f3506638092646fbaee2d6102407d4c07
JoshuaDavid <jshuadvd@icloud.com>
2015-03-04 18:36:51 -0800

    Made the first Sale

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -1,14 +1,10 @@
 class Sale < ActiveRecord::Base
-	
 	before_create :populate_guid
 	belongs_to :book
 	
-
 	private
-
+	
 	def populate_guid
-		self.guid = SquareRandom.uuid()
+		self.guid = SecureRandom.uuid()
 	end
-
-
-end
+end 
